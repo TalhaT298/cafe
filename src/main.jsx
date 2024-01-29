4b81647d354a00b70e1b43ea22d3dea2026f6998
Talha <mdtalha2008@gmail.com>
2024-01-29 19:41:39 +0600

    router setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {
   RouterProvider,
-} 
+} from "react-router-dom"
+import { router } from './Routes/Routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
