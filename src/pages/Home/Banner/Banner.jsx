d3f6e0f3e8672f6e380d3ee624031362c374ccc7
Talha <mdtalha2008@gmail.com>
2024-02-01 23:36:28 +0600

    photo webp

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import img1 from "../../../assets/1.webp";
 import img2 from "../../../assets/2.webp";
-import img3 from "../../../assets/3.webp";
+import img3 from "../../../assets/3.webp"
 const Banner = () => {
     return (
         <div>
