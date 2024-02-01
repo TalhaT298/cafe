2ef0fb7c993aac80a1b8d26b87f7463aa55fe076
Talha <mdtalha2008@gmail.com>
2024-02-01 20:58:24 +0600

    carousel add

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+const Banner = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Banner;
