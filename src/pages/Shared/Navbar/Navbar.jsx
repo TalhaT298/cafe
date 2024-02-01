b44e3a10ac12c00d04f195f979b7503f916c017b
Talha <mdtalha2008@gmail.com>
2024-02-02 00:54:44 +0600

    if we need to banner and header same fixed z-10 bg-opacity-30 bg-black text-white

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100"> 
+            {/* fixed z-10 bg-opacity-30 bg-black text-white */}
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
