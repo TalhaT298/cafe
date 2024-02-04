52b406e7543af6abf2c6f0ac77cbc62aec22788b
Talha <mdtalha2008@gmail.com>
2024-02-04 15:50:55 +0600

    catagory add home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Catagory from '../Catagory/Catagory';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Catagory></Catagory>
         </div>
     );
 };
