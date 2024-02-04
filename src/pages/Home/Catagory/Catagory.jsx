0b36dbfcdbda50dc621e4ad478238f7affa22cf7
Talha <mdtalha2008@gmail.com>
2024-02-04 15:49:59 +0600

    sweipper add in div

--- src/pages/Home/Catagory/Catagory.jsx
+++ src/pages/Home/Catagory/Catagory.jsx
@@ -5,9 +5,28 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 const Catagory = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+        </>
     );
 };
 
