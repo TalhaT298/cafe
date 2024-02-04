a4df0ef0ae31c0a4ba88e06a75bf7ce6022c88bf
Talha <mdtalha2008@gmail.com>
2024-02-04 23:23:29 +0600

    hotel banner add

--- src/pages/Home/Catagory/Catagory.jsx
+++ src/pages/Home/Catagory/Catagory.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import slide1 from "../../../assets/11.jpg";
+import slide2 from "../../../assets/12.jpg";
 const Catagory = () => {
-    return (
-        <>
-            <Swiper
+  return (
+    <>
+      <Swiper
         slidesPerView={4}
         spaceBetween={30}
         centeredSlides={true}
@@ -16,8 +18,13 @@ const Catagory = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>
+            <img src={slide1} alt="" />
+            <h3 className="text 4xl uppercase text-center -mt-16 text-white">Salads</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+        <img src={slide2} alt="" />
+        </SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
@@ -26,8 +33,8 @@ const Catagory = () => {
         <SwiperSlide>Slide 8</SwiperSlide>
         <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
-        </>
-    );
+    </>
+  );
 };
 
 export default Catagory;
