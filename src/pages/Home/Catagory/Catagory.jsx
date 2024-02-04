733a8e9a5a9fcb9368678e183cec07a18251b5d5
Talha <mdtalha2008@gmail.com>
2024-02-04 15:49:05 +0600

    swiperSlide add

--- src/pages/Home/Catagory/Catagory.jsx
+++ src/pages/Home/Catagory/Catagory.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 const Catagory = () => {
