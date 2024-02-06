8f066939aa468f4d2f550c7bb65694f2e45c7850
Talha <mdtalha2008@gmail.com>
2024-02-06 20:48:13 +0600

    image paragraph add

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-
+import feature from "../../../assets/feature.webp"
 const Featured = () => {
   return (
     <section className="mb-12">
@@ -8,6 +8,17 @@ const Featured = () => {
         subHeading={"check it out"}
         heading={"Feature"}
       ></SectionTitle>
+      <div className="md:flex justify-center items-center py-8 px-16">
+      <div>
+        <img src={feature} alt="" />
+      </div>
+      <div className="md:ml-10">
+        <p>Aug 20,2024</p>
+        <p className="uppercase">Where I can get Some</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        <button className="btn btn-outline">Default</button>
+      </div>
+      </div>
     </section>
   );
 };
