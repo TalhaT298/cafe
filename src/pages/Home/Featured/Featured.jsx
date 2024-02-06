16da0db8a5ef743cd786058c5f6cc88da3752663
Talha <mdtalha2008@gmail.com>
2024-02-06 23:33:14 +0600

    testimonial add

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,7 +4,7 @@ import feature from "../../../assets/feature.webp"
 import './Featured.css';
 const Featured = () => {
   return (
-    <section className="featured-item text-white mb-12 pt-6 my-10">
+    <section className="featured-item bg-fixed text-white mb-12 pt-6 my-20">
       <SectionTitle
         subHeading={"check it out"}
         heading={"Feature"}
@@ -17,7 +17,7 @@ const Featured = () => {
         <p>Aug 20,2024</p>
         <p className="uppercase">Where I can get Some</p>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-        <button className="btn btn-outline">Default</button>
+        <button className="btn btn-outline border-0 border-b-4 text-white">Read More</button>
       </div>
       </div>
     </section>
