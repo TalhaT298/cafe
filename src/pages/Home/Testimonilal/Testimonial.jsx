62c004b7ca8b9e2045b6495db5b5e97a03600504
Talha <mdtalha2008@gmail.com>
2024-02-06 23:34:19 +0600

    sctiontitle add

--- src/pages/Home/Testimonilal/Testimonial.jsx
+++ src/pages/Home/Testimonilal/Testimonial.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Testimonial = () => {
     return (
         <section>
-            
+            <SectionTitle
+        subHeading={"check it out"}
+        heading={"Feature"}
+      ></SectionTitle>
         </section>
     );
 };
