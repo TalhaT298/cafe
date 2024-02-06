bec05433a476dc0ff0c889816852d7be9041b57f
Talha <mdtalha2008@gmail.com>
2024-02-06 19:55:55 +0600

    subheading add

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Featured = () => {
-    return (
-        <div>
-            g
-        </div>
-    );
+  return (
+    <section className="mb-12">
+      <SectionTitle
+        subHeading={"From Our Menu"}
+        heading={"Popular item"}
+      ></SectionTitle>
+    </section>
+  );
 };
 
 export default Featured;
