1c354a85bea687729b0fe6524c395f7547275d19
Talha <mdtalha2008@gmail.com>
2024-02-06 23:45:51 +0600

    swipper add testimonial

--- src/pages/Home/Testimonilal/Testimonial.jsx
+++ src/pages/Home/Testimonilal/Testimonial.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 const Testimonial = () => {
   return (
