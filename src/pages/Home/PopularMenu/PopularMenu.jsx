5db81ff16d3bf8caad515ebc07b3eaf7fb802801
Talha <mdtalha2008@gmail.com>
2024-02-06 07:48:13 +0600

    setMenu add useeffect add

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PopularMenu = () => {
+    const [menu,setMenu]=useState([]);
+    useEffect(()=>{
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data =>setMenu(data) )
+    },[])
     return (
         <section>
             <SectionTitle
