55490f9c74d657662938f8c1b158653cc880e618
Talha <mdtalha2008@gmail.com>
2024-02-07 00:49:34 +0600

    reviw.rating add

--- src/pages/Home/Testimonilal/Testimonial.jsx
+++ src/pages/Home/Testimonilal/Testimonial.jsx
@@ -6,14 +6,15 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
-
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
 const Testimonial = () => {
-    const [reviews,setReviews]= useState([]);
-    useEffect(()=>{
-        fetch('reviews.json')
-        .then(res => res.json())
-        .then(data => setReviews(data))
-    },[])
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
   return (
     <section className="my-20">
       <SectionTitle
@@ -22,19 +23,15 @@ const Testimonial = () => {
       ></SectionTitle>
 
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        
-        {
-            reviews.map(review => <SwiperSlide
-            key={review._id}
-            
-            >
-                <div className="m-24">
-                    <p>{review.details}</p>
-                    <h3 className="text-2xl text-orange-400">{review.name}</h3>
-                </div>
-
-            </SwiperSlide>)
-        }
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className="m-24">
+              <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+              <p>{review.details}</p>
+              <h3 className="text-2xl text-orange-400">{review.name}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
