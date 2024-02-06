5a91c84b8c2c4af047bb601506baab75afbf248d
Talha <mdtalha2008@gmail.com>
2024-02-06 07:41:20 +0600

    popular menu add at homepage

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Catagory from '../Catagory/Catagory';
+import PopularMenu from '../PopularMenu/PopularMenu';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Catagory></Catagory>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
