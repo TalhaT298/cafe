6b60e1c2e46876b86aab655ca5fc91338279674f
Talha <mdtalha2008@gmail.com>
2024-02-06 07:30:25 +0600

    mergin add

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SectionTitle = ({heading,subHeading}) => {
     return (
-        <div className='md:w-3/12'>
+        <div className='mx-auto text-center md:w-4/12 my-8'>
             <p className='text-yellow-600'>---{subHeading}--</p>
             <h3 className='text-4xl uppercase border-y-4 py-4'>{heading}</h3>
         </div>
