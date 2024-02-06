14f36db64c22d9ad6d2d38ead5b43d2c336a3191
Talha <mdtalha2008@gmail.com>
2024-02-06 08:02:30 +0600

    heading and paragraph add

--- src/pages/Shared/MenuItem/MenuItem.jsx
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -5,6 +5,11 @@ const MenuItem = ({item}) => {
     return (
         <div>
             <img src={image} alt="" />
+            <div>
+                <h3>{name}-------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p>{price}</p>
         </div>
     );
 };
