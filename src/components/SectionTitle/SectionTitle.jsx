37feabf9753a7c54e52c6c5d72846a240a366616
Talha <mdtalha2008@gmail.com>
2024-02-06 07:27:43 +0600

    border add

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -4,7 +4,7 @@ const SectionTitle = ({heading,subHeading}) => {
     return (
         <div>
             <p className='text-yellow-600'>---{subHeading}--</p>
-            <h3 className='text-4xl uppercase'>{heading}</h3>
+            <h3 className='text-4xl uppercase border-y-4'>{heading}</h3>
         </div>
     );
 };
