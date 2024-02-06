574deb99c29c90896d0da4e73616a5b26f240b55
Talha <mdtalha2008@gmail.com>
2024-02-06 14:08:49 +0600

    chef add at home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Catagory from '../Catagory/Catagory';
 import PopularMenu from '../PopularMenu/PopularMenu';
+import Chef from '../Chef/Chef';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Catagory></Catagory>
             <PopularMenu></PopularMenu>
+            <Chef></Chef>
         </div>
     );
 };
