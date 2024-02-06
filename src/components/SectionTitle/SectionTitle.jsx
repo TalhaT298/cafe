732be3bbce4d9f2865756d7743e81ff0ef2a1ec5
Talha <mdtalha2008@gmail.com>
2024-02-06 07:29:04 +0600

    width add

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SectionTitle = ({heading,subHeading}) => {
     return (
-        <div>
+        <div className='md:w-3/12'>
             <p className='text-yellow-600'>---{subHeading}--</p>
-            <h3 className='text-4xl uppercase border-y-4'>{heading}</h3>
+            <h3 className='text-4xl uppercase border-y-4 py-4'>{heading}</h3>
         </div>
     );
 };
