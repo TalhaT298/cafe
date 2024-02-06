d29dfe2c9b47bfc6973b15db4044b3caa257ad61
Talha <mdtalha2008@gmail.com>
2024-02-06 14:10:19 +0600

    chef sectiotitle add

--- src/pages/Home/Chef/Chef.jsx
+++ src/pages/Home/Chef/Chef.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Chef = () => {
     return (
         <section>
-            
+            <SectionTitle
+          subHeading={"From 11.00am to 10.00pm"}
+          heading={"Order Now"}
+        ></SectionTitle>
         </section>
     );
 };
