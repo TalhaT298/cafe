18fe525a73fd38ef0756fce110dbc6899fa0bec2
Talha <mdtalha2008@gmail.com>
2024-02-06 23:48:08 +0600

    swipper add

--- src/pages/Home/Testimonilal/Testimonial.jsx
+++ src/pages/Home/Testimonilal/Testimonial.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 
 const Testimonial = () => {
   return (
@@ -14,6 +14,18 @@ const Testimonial = () => {
         subHeading={"What our Client Say"}
         heading={"Testimonials"}
       ></SectionTitle>
+
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </section>
   );
 };
