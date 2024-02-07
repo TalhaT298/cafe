1b8d1b1125d83b874a537a2b180fea0ba93654c6
Talha <mdtalha2008@gmail.com>
2024-02-07 21:40:41 +0600

    icon add

--- src/pages/Home/Testimonilal/Testimonial.jsx
+++ src/pages/Home/Testimonilal/Testimonial.jsx
@@ -8,6 +8,7 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { Rating } from "@smastrom/react-rating";
 import '@smastrom/react-rating/style.css'
+import { TfiThought } from "react-icons/tfi";
 const Testimonial = () => {
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
@@ -26,6 +27,7 @@ const Testimonial = () => {
         {reviews.map((review) => (
           <SwiperSlide key={review._id}>
             <div className="flex flex-col items-center mx-24 my-16">
+            <TfiThought size='1.7rem' color='black'></TfiThought>
               <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
               <p className="py-8">{review.details}</p>
               <h3 className="text-2xl text-orange-400">{review.name}</h3>
