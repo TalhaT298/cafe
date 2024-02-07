bd77a318132c262cbb407694731026457601e4b2
Talha <mdtalha2008@gmail.com>
2024-02-08 02:48:04 +0600

    thoughts add

--- src/pages/Home/Testimonilal/Testimonial.jsx
+++ src/pages/Home/Testimonilal/Testimonial.jsx
@@ -27,7 +27,7 @@ const Testimonial = () => {
         {reviews.map((review) => (
           <SwiperSlide key={review._id}>
             <div className="flex flex-col items-center mx-24 my-16">
-            <TfiThought size='1.7rem' color='black'></TfiThought>
+            <TfiThought size='1.7rem' color='white'></TfiThought>
               <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
               <p className="py-8">{review.details}</p>
               <h3 className="text-2xl text-orange-400">{review.name}</h3>
