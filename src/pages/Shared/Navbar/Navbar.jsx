67a285d5318cad8ff82f536573dce738127ad224
Talha <mdtalha2008@gmail.com>
2024-02-09 02:13:39 +0600

    link add at navbar

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const navOptions = <>
-      <li><a href=""></a></li>
+      <li><Link></Link></li>
       <li><a href=""></a></li>
 
 </>
