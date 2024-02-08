f991ed5ccb1f4cf4a0e5848d58c48e0502bbccee
Talha <mdtalha2008@gmail.com>
2024-02-09 02:34:25 +0600

    navbar update

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,57 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const navOptions = <>
-      <li><Link></Link></li>
-      <li><a href=""></a></li>
-
-</>
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            <div className="navbar bg-base-100 max-w-screen-xl"> 
-            {/* fixed z-10 bg-opacity-30 bg-black text-white */}
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu </a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">UI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
+
+  const navOption = (
+    <>
       <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
+        <Link to="/">Home</Link>
       </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
+      <li>
+        <Link to="/menu">Our Menu</Link>
+      </li>
+
+    </>
+  );
+
+  return (
+    <div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl text-white bg-black">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu font-medium text-black  menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-32"
+          >
+            {navOption}
+          </ul>
         </div>
-    );
+        <a className="btn btn-ghost text-xl">javaJive</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal font-medium items-center  px-1">{navOption}</ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
