6873219280d979e1473c2c9fdfd0c72b0238c6a1
Talha <mdtalha2008@gmail.com>
2024-02-10 20:56:06 +0600

    title dynamic

--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Cover = ({img}) => {
+const Cover = ({img,title}) => {
   return (
     <div
       className="hero h-[500px]"
@@ -12,7 +12,7 @@ const Cover = ({img}) => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">{title}</h1>
           <p className="mb-5">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
