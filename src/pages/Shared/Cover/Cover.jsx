7941293900b2be7c27d545dc6d2aff90977c7f48
Talha <mdtalha2008@gmail.com>
2024-02-10 22:46:05 +0600

    parallax fixed

--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -1,38 +1,32 @@
 import React from "react";
-import { Parallax } from 'react-parallax';
-const Cover = ({img,title}) => {
+import { Parallax } from "react-parallax";
+const Cover = ({ img, title }) => {
   return (
-
     <Parallax
-        blur={{ min: -50, max: 50 }}
-        bgImage={img}
-        bgImageAlt="the menu"
-        strength={-200}
+      blur={{ min: -100, max: 100 }}
+      bgImage={img}
+      bgImageAlt="the menu"
+      strength={-200}
     >
-        <div
-      className="hero h-[500px]"
-      style={{
-        backgroundImage:
-          `url("${img}")`,
-      }}
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          
+      <div
+        className="hero h-[500px]"
+        style={{
+          backgroundImage: `url("${img}")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
     </Parallax>
-
-
-    
   );
 };
 
