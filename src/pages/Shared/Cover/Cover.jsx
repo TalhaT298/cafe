dca15b60be82d026fbf6b646bbc964132278a114
Talha <mdtalha2008@gmail.com>
2024-02-10 20:58:34 +0600

    uppercase add

--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -12,7 +12,7 @@ const Cover = ({img,title}) => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+          <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
           <p className="mb-5">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
