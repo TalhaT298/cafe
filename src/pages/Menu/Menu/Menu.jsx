4a80e9e2d548ba3b41535a8e0980acdaf11fd955
Talha <mdtalha2008@gmail.com>
2024-02-11 20:07:49 +0600

    desert add

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu.webp'
+import useMenu from '../../../hooks/useMenu';
 const Menu = () => {
+    const [menu]= useMenu()
+    const dessert = menu.filter((item) => item.category === "dessert");
     return (
         <div>
             <Helmet>
