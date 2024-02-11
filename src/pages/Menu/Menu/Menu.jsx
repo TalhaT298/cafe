6254796d83e17e039aa70f6a2abec8cb6c68faf7
Talha <mdtalha2008@gmail.com>
2024-02-11 20:25:58 +0600

    subheading add

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu.webp'
 import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 const Menu = () => {
     const [menu]= useMenu()
     const dessert = menu.filter((item) => item.category === "dessert");
@@ -16,7 +17,7 @@ const Menu = () => {
                 <title>javaJive |Menu</title>
             </Helmet>
             <Cover img={menuImg} title='our menu'></Cover>
-            
+            <SectionTitle subHeading="Don't Miss"></SectionTitle>
         </div>
     );
 };
