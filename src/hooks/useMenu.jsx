842f68a3dffefe3a8ae2f9a595fafa230b20ce61
Talha <mdtalha2008@gmail.com>
2024-02-11 18:38:08 +0600

    useState and useEffect add

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,5 +1,14 @@
-const useMenu = () =>{
+import { useEffect, useState } from "react";
 
+const useMenu = () =>{
+    const [menu,setMenu]=useState([]);
+    useEffect(()=>{
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data =>{
+            const popularItems=data.filter(item=> item.category==='popular');
+            setMenu(popularItems)} )
+    },[])
 }
 
 export default useMenu;
