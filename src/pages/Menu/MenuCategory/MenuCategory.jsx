d34721034da244fc0412d985235509a54d27fc1b
Talha <mdtalha2008@gmail.com>
2024-02-11 20:59:47 +0600

    coverImg add

--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -5,7 +5,7 @@ import Cover from '../../Shared/Cover/Cover';
 const MenuCategory = ({items,title,coverImg}) => {
     return (
         <div>
-            {title && <Cover img={menuImg} title='our menu'></Cover>}
+            {title && <Cover img={coverImg} title='our menu'></Cover>}
             <div className="grid md:grid-cols-2 gap-10">
         {items.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
