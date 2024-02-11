310747867af32f4987621169df9740c94e7d5822
Talha <mdtalha2008@gmail.com>
2024-02-11 18:48:11 +0600

    setLoading false

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -2,13 +2,18 @@ import { useEffect, useState } from "react";
 
 const useMenu = () =>{
     const [menu,setMenu]=useState([]);
+    const [loading,setLoading] = useState(true)
     useEffect(()=>{
         fetch('menu.json')
         .then(res => res.json())
         .then(data =>{
-            const popularItems=data.filter(item=> item.category==='popular');
-            setMenu(popularItems)} )
+            setMenu(data);
+            setLoading(false);
+        
+        
+        } );
     },[])
+    return [menu]
 }
 
 export default useMenu;
