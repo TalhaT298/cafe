5383a3d13f15c2fc002f36b1b4eeffe80d6bc1c8
Talha <mdtalha2008@gmail.com>
2024-02-11 20:20:52 +0600

    offered add

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -9,6 +9,7 @@ const Menu = () => {
     const soup = menu.filter((item) => item.category === "soup");
     const salad = menu.filter((item) => item.category === "salad");
     const pizza = menu.filter((item) => item.category === "pizza");
+    const offered = menu.filter((item) => item.category === "offered");
     return (
         <div>
             <Helmet>
