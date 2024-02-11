59d6a21659bf9933677775e2e20b52c0dc01258c
Talha <mdtalha2008@gmail.com>
2024-02-11 20:46:59 +0600

    offerd

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -4,6 +4,7 @@ import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu.webp'
 import useMenu from '../../../hooks/useMenu';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 const Menu = () => {
     const [menu]= useMenu()
     const dessert = menu.filter((item) => item.category === "dessert");
@@ -18,6 +19,7 @@ const Menu = () => {
             </Helmet>
             <Cover img={menuImg} title='our menu'></Cover>
             <SectionTitle subHeading="Don't Miss" heading="Toda's Offer"></SectionTitle>
+            <MenuCategory items={offered}></MenuCategory>
         </div>
     );
 };
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import MenuItem from '../../Shared/MenuItem/MenuItem';
 
-const MenuCategory = () => {
+const MenuCategory = ({items}) => {
     return (
         <div>
-            
+            <div className="grid md:grid-cols-2 gap-10">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
         </div>
     );
 };
