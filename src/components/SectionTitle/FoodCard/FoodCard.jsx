3e45a215cba80aa47d1e90ee86f7701c0bd641b3
Talha <mdtalha2008@gmail.com>
2024-02-13 23:02:13 +0600

    pricing add

--- src/components/SectionTitle/FoodCard/FoodCard.jsx
+++ src/components/SectionTitle/FoodCard/FoodCard.jsx
@@ -8,7 +8,7 @@ const FoodCard = ({ item }) => {
         <figure>
           <img src={image} alt="Shoes" />
         </figure>
-        <p>${price}</p>
+        <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
         <div className="card-body text-center">
           <h2 className="card-title">{name}</h2>
           <p>{recipe}</p>
