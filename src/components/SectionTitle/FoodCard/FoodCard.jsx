176b6f189e9596f6b0c56c9d5b361ed7f83ee716
Talha <mdtalha2008@gmail.com>
2024-02-13 19:46:25 +0600

    foodcard add

---
+++ src/components/SectionTitle/FoodCard/FoodCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const FoodCard = (item) => {
+    const {name,image,price,recipe}=item;
+    return (
+        <div>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src={image} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default FoodCard;
--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -3,17 +3,30 @@ import Cover from "../../Shared/Cover/Cover";
 import orderCover from "../../../assets/ordercover.webp";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import useMenu from "../../../hooks/useMenu";
 
 const Order = () => {
-    const [tabIndex,setTabIndex]=useState(0)
+    const [tabIndex,setTabIndex]=useState(0);
+    const [menu]=useMenu();
+    const desserts = menu.filter((item) => item.category === "dessert");
+    const soup = menu.filter((item) => item.category === "soup");
+    const salad = menu.filter((item) => item.category === "salad");
+    const pizza = menu.filter((item) => item.category === "pizza");
+    const offered = menu.filter((item) => item.category === "offered");
   return (
     <div>
       <Cover img={orderCover} title="Order Food"></Cover>
       <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
       <TabList>
-        <Tab>Title 1</Tab>
-        <Tab>Title 2</Tab>
+        <Tab>Salad</Tab>
+        <Tab>Pizza</Tab>
+        <Tab>Soup</Tab>
+        <Tab>Dessert</Tab>
+        <Tab>Drinks</Tab>
       </TabList>
+      <TabPanel></TabPanel>
+      <TabPanel></TabPanel>
+      <TabPanel></TabPanel>
       <TabPanel></TabPanel>
       <TabPanel></TabPanel>
     </Tabs>
