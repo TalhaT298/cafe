eb6e2e625594d490913a336a757d30e2319b3aee
Talha <mdtalha2008@gmail.com>
2024-02-13 18:42:14 +0600

    tab import

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Cover from "../../Shared/Cover/Cover";
 import orderCover from "../../../assets/ordercover.webp";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 const Order = () => {
   return (
     <div>
