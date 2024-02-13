1a0ba29f2ebdb7cd82374f4846198e8d5caa7ae4
Talha <mdtalha2008@gmail.com>
2024-02-13 18:34:43 +0600

    tab add

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import Cover from '../../Shared/Cover/Cover';
-import orderCover from '../../../assets/ordercover.webp';
+import React from "react";
+import Cover from "../../Shared/Cover/Cover";
+import orderCover from "../../../assets/ordercover.webp";
 const Order = () => {
-    return (
-        <div>
-            <Cover img={orderCover} title="Order Food"></Cover>
-        </div>
-    );
+  return (
+    <div>
+      <Cover img={orderCover} title="Order Food"></Cover>
+      <div role="tablist" className="tabs tabs-lifted">
+        <a role="tab" className="tab">
+          Tab 1
+        </a>
+        <a role="tab" className="tab tab-active">
+          Tab 2
+        </a>
+        <a role="tab" className="tab">
+          Tab 3
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Order;
