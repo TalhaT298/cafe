0d840cfe3284b56b5fd86c03a7ed9816122b7e1f
Talha <mdtalha2008@gmail.com>
2024-02-13 18:45:21 +0600

    tab listed

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -8,17 +8,14 @@ const Order = () => {
   return (
     <div>
       <Cover img={orderCover} title="Order Food"></Cover>
-      <div role="tablist" className="tabs tabs-lifted">
-        <a role="tab" className="tab">
-          Tab 1
-        </a>
-        <a role="tab" className="tab tab-active">
-          Tab 2
-        </a>
-        <a role="tab" className="tab">
-          Tab 3
-        </a>
-      </div>
+      <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+      <TabList>
+        <Tab>Title 1</Tab>
+        <Tab>Title 2</Tab>
+      </TabList>
+      <TabPanel></TabPanel>
+      <TabPanel></TabPanel>
+    </Tabs>
     </div>
   );
 };
