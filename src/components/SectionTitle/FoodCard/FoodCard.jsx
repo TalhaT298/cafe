36488af9d925f7cd8b5845eab71642e770c4b294
Talha <mdtalha2008@gmail.com>
2024-02-13 19:48:33 +0600

    name add

--- src/components/SectionTitle/FoodCard/FoodCard.jsx
+++ src/components/SectionTitle/FoodCard/FoodCard.jsx
@@ -7,7 +7,7 @@ const FoodCard = (item) => {
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
   <figure><img src={image} alt="Shoes" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
+    <h2 className="card-title">{name}</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Buy Now</button>
