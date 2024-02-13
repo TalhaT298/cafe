8efabe90fd351246c07ac6e89fd86570f1814461
Talha <mdtalha2008@gmail.com>
2024-02-13 22:52:14 +0600

    price add

--- src/components/SectionTitle/FoodCard/FoodCard.jsx
+++ src/components/SectionTitle/FoodCard/FoodCard.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const FoodCard = ({item}) => {
-    const {name,image,price,recipe}=item;
-    return (
-        <div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src={image} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">{name}</h2>
-    <p>{recipe}</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Add to Cart</button>
-    </div>
-  </div>
-</div>
+const FoodCard = ({ item }) => {
+  const { name, image, price, recipe } = item;
+  return (
+    <div>
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img src={image} alt="Shoes" />
+        </figure>
+        <p>${price}</p>
+        <div className="card-body text-center">
+          <h2 className="card-title">{name}</h2>
+          <p>{recipe}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Add to Cart</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FoodCard;
