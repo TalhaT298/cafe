84bb02facfd5a89a9f409bf7f79f27d3020e5404
Talha <mdtalha2008@gmail.com>
2024-02-15 12:02:33 +0600

    from update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,11 +11,11 @@ const Login = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
+    <div className="text-center md:w-1/2 lg:text-left">
       <h1 className="text-5xl font-bold">Login now!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
-    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+    <div className="card w-full md:w-1/2 max-w-sm shadow-2xl bg-base-100">
       <form onSubmit={hanleLogin} className="card-body">
         <div className="form-control">
           <label className="label">
