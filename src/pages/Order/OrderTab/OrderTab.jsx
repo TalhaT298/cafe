fc72ca7c5ae5d3668474ccbc2a0b38d486ed99d1
Talha <mdtalha2008@gmail.com>
2024-02-15 11:06:15 +0600

    login add

--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -8,9 +8,23 @@ import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 
 const OrderTab = ({items}) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      };
     return (
         <div>
-           <div className="grid md:grid-cols-3 gap-10">
+           <div >
+       
+        <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        <div className="grid md:grid-cols-3 gap-10">
         {
             items.map(item => <FoodCard
             key={item._id}
@@ -18,6 +32,10 @@ const OrderTab = ({items}) => {
             
             ></FoodCard>)
         }
+        </div>
+        </SwiperSlide>
+       
+      </Swiper>
         </div> 
         </div>
     );
