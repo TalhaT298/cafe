6fdd7a4454b6e89006074ae6bc5160cd4cba3fc4
Talha <mdtalha2008@gmail.com>
2024-02-20 03:46:00 +0600

    sweetalert2 import

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,6 +7,7 @@ import {
 import { AuthContext } from "../../providers/AuthProvider";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import Swal from 'sweetalert2'
 const Login = () => {
   const captchaRef = useRef(null);
   const [disabled, setDisabled] = useState(true);
