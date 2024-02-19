449883497fa0d87ac2bee400b47ccf22e1db13da
Talha <mdtalha2008@gmail.com>
2024-02-20 03:16:06 +0600

    useContext

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const {user}=useContext(AudioContext);
   const navOption = (
     <>
       <li>
