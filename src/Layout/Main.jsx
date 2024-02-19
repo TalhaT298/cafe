b0ba680eb6ab3f153d94cd2612bfb40970621a57
Talha <mdtalha2008@gmail.com>
2024-02-19 21:09:51 +0600

    signup add

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -5,7 +5,7 @@ import Navbar from '../pages/Shared/Navbar/Navbar';
 
 const Main = () => {
     const location = useLocation();
-    console.log(location);
+    // console.log(location);
     const noHeaderFooter= location.pathname.includes('login') || location.pathname.includes('signup');
     return (
         <div>
