66220d5b43fe1218563093c187b269cf4a9e5394
Talha <mdtalha2008@gmail.com>
2024-02-20 02:56:18 +0600

    sighnup succes

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -8,6 +8,11 @@ const SignUp = () => {
   const {createUser}= useContext(AuthContext);
   const onSubmit = (data) => {
     console.log(data);
+    createUser(data.email, data.password)
+    .then(result =>{
+        const loggedUser = result.user;
+        console.log(loggedUser);
+    })
   };
 
   return (
