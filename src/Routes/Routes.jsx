465bd45966deac39eff86c1928ee08a0184893fd
Talha <mdtalha2008@gmail.com>
2024-02-19 20:40:07 +0600

    signup add at router

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,6 +7,7 @@ import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
 import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
   
   export const router = createBrowserRouter([
@@ -29,6 +30,10 @@ import Login from "../pages/Login/Login";
         {
           path: '/login',
           element:<Login></Login>
+        },
+        {
+          path: '/signup',
+          element:<SignUp></SignUp>
         },
       ]
     },
