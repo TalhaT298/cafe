58cd544c7522e6cd8520796ab01f6ce0655fdb00
Talha <mdtalha2008@gmail.com>
2024-02-19 18:22:07 +0600

    config js add

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyNAyK4LdF2noTpqxR8qneCIYiNr_zB9Q",
+  authDomain: "javajive-d24d3.firebaseapp.com",
+  projectId: "javajive-d24d3",
+  storageBucket: "javajive-d24d3.appspot.com",
+  messagingSenderId: "923911919232",
+  appId: "1:923911919232:web:51d09afbb4a2e6c73e3865"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
