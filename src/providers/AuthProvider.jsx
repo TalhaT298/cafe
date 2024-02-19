f2b51634cc97ba0afc2e32e3f4f5ba71220fb03c
Talha <mdtalha2008@gmail.com>
2024-02-19 19:29:42 +0600

    setlogin add

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,15 +1,15 @@
-import React, { Children, createContext } from 'react';
+import React, { Children, createContext, useState } from "react";
 
 export const AuthContext = createContext(null);
 const AuthProvider = () => {
-    const authInfo={
-
-    }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const authInfo = {
+    user,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
