0e97a29c9704e5fdd710b93c414596be15b575e1
Talha <mdtalha2008@gmail.com>
2024-02-20 23:00:23 +0600

    navigate add

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
+import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
-    const {user}= useContext(AuthContext)
-    return (
-        <div>
-            
-        </div>
-    );
+    const {user}= useContext(AuthContext);
+    if (user){
+        return children;
+    }
+    return <Navigate></Navigate>
 };
 
 export default PrivateRoute;
