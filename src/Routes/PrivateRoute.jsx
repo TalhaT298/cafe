dd8bd9027cfcd2d8489abafbac67bcdccdb3d4eb
Talha <mdtalha2008@gmail.com>
2024-02-21 19:30:52 +0600

    location add

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -11,7 +11,7 @@ const PrivateRoute = ({children}) => {
     if (user){
         return children;
     }
-    return <Navigate to="/login"></Navigate>
+    return <Navigate to="/login" state={{from: location}} replace></Navigate>
 };
 
 export default PrivateRoute;
