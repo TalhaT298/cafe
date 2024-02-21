315cddfb25980c76b4e58c445daec0b4e5743581
Talha <mdtalha2008@gmail.com>
2024-02-21 19:29:08 +0600

    location add

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
     const {user,loading}= useContext(AuthContext);
+    const location = useLocation
     if(loading){
         return<progress className="progress w-56"></progress>
     }
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -20,6 +20,9 @@ const Navbar = () => {
       <li>
         <Link to="/order/salad">Order Food</Link>
       </li>
+      <li>
+        <Link to="/secret">Secret</Link>
+      </li>
       {/* {
         user?  <>
          <button onClick={handleLogOut} className="btn btn-ghost">Button</button>
