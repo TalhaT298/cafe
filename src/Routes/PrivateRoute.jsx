31a57da53f647d310d96ec105477685755c3f4f8
Talha <mdtalha2008@gmail.com>
2024-02-21 19:04:53 +0600

    profress add

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({children}) => {
     const {user,loading}= useContext(AuthContext);
     if(loading){
-        return
+        return<progress className="progress w-56"></progress>
     }
     if (user){
         return children;
